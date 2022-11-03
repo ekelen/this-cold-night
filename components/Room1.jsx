deb1bb4c48585524c8f03dea71e3a68d608bf641
ekelen <erskelen@gmail.com>
2022-11-03 19:45:59 -0400

    split animation hook

--- components/Room1.jsx
+++ components/Room1.jsx
@@ -1,12 +1,13 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import { cellLen, getPath, grid, items, pxPerFrame } from "../game/setup";
 import useGame from "../game/useGame";
+import useAnimation from "../hooks/useAnimation";
 import styles from "../styles/Home.module.css";
 import Modal from "./Modal";
 
 export default function Room1() {
   const [gameState, { move, reset }] = useGame();
-  const [nMoves, setNMoves] = useState(0);
+
   const [showModal, setShowModal] = useState(false);
 
   const {
@@ -27,122 +28,14 @@ export default function Room1() {
   const charRef = useRef();
   const gridRef = useRef();
 
-  useEffect(() => {
-    return () => requestRef.current && cancelAnimationFrame(requestRef.current);
-  }, []);
-
-  const moveToAnim =
-    ({ path, dirX, dirY, count, startCharLeft, startCharTop }) =>
-    (time) => {
-      if (previousTimeRef.current != undefined) {
-        if (count > 10000) {
-          console.error("Too many iterations");
-          resetAnimation();
-          return;
-        }
-
-        const [_targetJ, _targetI] = path[0];
-
-        const charLeft = parseInt(charRef.current.style.left);
-        const charTop = parseInt(charRef.current.style.top);
-
-        const atX = charLeft === _targetJ * cellLen;
-        const atY = charTop === _targetI * cellLen;
-
-        let newDirY, newDirX;
-        if (atX && atY) {
-          if (path.length === 1) {
-            move({
-              j: Math.floor(charLeft / cellLen),
-              i: Math.floor(charTop / cellLen),
-            });
-            cancelAnimation();
-            return;
-          }
-          const [_nextTargetJ, _nextTargetI] = path[1];
-          newDirX = Math.sign(_nextTargetJ - _targetJ);
-          newDirY = Math.sign(_nextTargetI - _targetI);
-          requestRef.current = requestAnimationFrame(
-            moveToAnim({
-              path: path.slice(1),
-              dirX: newDirX,
-              dirY: newDirY,
-              count: 0,
-              startCharLeft: charLeft,
-              startCharTop: charTop,
-            })
-          );
-        } else {
-          charRef.current.style.left = `${startCharLeft + count * dirX}px`;
-          charRef.current.style.top = `${startCharTop + count * dirY}px`;
-          requestRef.current = requestAnimationFrame(
-            moveToAnim({
-              path,
-              dirX,
-              dirY,
-              count: count + pxPerFrame,
-              startCharLeft,
-              startCharTop,
-            })
-          );
-        }
-      } else {
-        previousTimeRef.current = time;
-        gridRef.current.style.pointerEvents = "none";
-        requestRef.current = requestAnimationFrame(
-          moveToAnim({ path, dirX, dirY, count, startCharLeft, startCharTop })
-        );
-      }
-    };
-
-  const startAnimation = (_currentJ, _currentI, _targetJ, _targetI, node) => {
-    if (charRef.current && gridRef.current) {
-      const dirX = Math.sign(_targetJ - _currentJ);
-      const dirY = Math.sign(_targetI - _currentI);
-      if (dirX === 0 && dirY === 0) {
-        console.log(`[=] you are already there!`);
-        return;
-      }
-      if (!node.walkable) {
-        console.log(`[=] node not walkable!`);
-        return;
-      }
-
-      const startCharLeft = parseInt(charRef.current.style.left);
-      const startCharTop = parseInt(charRef.current.style.top);
-
-      const path = getPath(_currentJ, _currentI, _targetJ, _targetI);
-      setNMoves((prevMoves) => prevMoves + path.length - 1);
-      requestRef.current = requestAnimationFrame(
-        moveToAnim({
-          path,
-          dirX,
-          dirY,
-          count: 0,
-          startCharLeft,
-          startCharTop,
-        })
-      );
-    }
-  };
-
-  const cancelAnimation = () => {
-    console.log(`[=] cancelling animation`);
-    cancelAnimationFrame(requestRef.current);
-    gridRef.current.style.pointerEvents = "auto";
-    previousTimeRef.current = undefined;
-  };
-
-  const resetAnimation = () => {
-    console.log(`[=] cancelling animation`);
-    cancelAnimationFrame(requestRef.current);
-    gridRef.current.style.pointerEvents = "auto";
-    previousTimeRef.current = undefined;
-    charRef.current.style.left = "0px";
-    charRef.current.style.top = "0px";
-    reset();
-    setNMoves(0);
-  };
+  const { resetAnimation, startAnimation, nMoves } = useAnimation(
+    charRef,
+    gridRef,
+    requestRef,
+    previousTimeRef,
+    reset,
+    move
+  );
 
   return (
     <>
---
+++ hooks/useAnimation.jsx
@@ -0,0 +1,128 @@
+import { useState } from "react";
+import { cellLen, getPath, pxPerFrame } from "../game/setup";
+
+const useAnimation = (
+  charRef,
+  gridRef,
+  requestRef,
+  previousTimeRef,
+  reset,
+  move
+) => {
+  const [nMoves, setNMoves] = useState(0);
+
+  const cancelAnimation = () => {
+    cancelAnimationFrame(requestRef.current);
+    gridRef.current.style.pointerEvents = "auto";
+    previousTimeRef.current = undefined;
+  };
+
+  const resetAnimation = () => {
+    cancelAnimationFrame(requestRef.current);
+    gridRef.current.style.pointerEvents = "auto";
+    previousTimeRef.current = undefined;
+    charRef.current.style.left = "0px";
+    charRef.current.style.top = "0px";
+    reset();
+    setNMoves(0);
+  };
+
+  const moveToAnim =
+    ({ path, dirX, dirY, count, startCharLeft, startCharTop }) =>
+    (time) => {
+      if (previousTimeRef.current != undefined) {
+        const [endX, endY] = path[0];
+
+        const charLeft = parseInt(charRef.current.style.left);
+        const charTop = parseInt(charRef.current.style.top);
+
+        const atX = charLeft === endX * cellLen;
+        const atY = charTop === endY * cellLen;
+
+        let newDirY, newDirX;
+        if (atX && atY) {
+          if (path.length === 1) {
+            move({
+              j: Math.floor(charLeft / cellLen),
+              i: Math.floor(charTop / cellLen),
+            });
+            cancelAnimation();
+            return;
+          }
+          const [nextEndX, nextEndY] = path[1];
+          newDirX = Math.sign(nextEndX - endX);
+          newDirY = Math.sign(nextEndY - endY);
+          requestRef.current = requestAnimationFrame(
+            moveToAnim({
+              path: path.slice(1),
+              dirX: newDirX,
+              dirY: newDirY,
+              count: 0,
+              startCharLeft: charLeft,
+              startCharTop: charTop,
+            })
+          );
+        } else {
+          charRef.current.style.left = `${startCharLeft + count * dirX}px`;
+          charRef.current.style.top = `${startCharTop + count * dirY}px`;
+          requestRef.current = requestAnimationFrame(
+            moveToAnim({
+              path,
+              dirX,
+              dirY,
+              count: count + pxPerFrame,
+              startCharLeft,
+              startCharTop,
+            })
+          );
+        }
+      } else {
+        previousTimeRef.current = time;
+        gridRef.current.style.pointerEvents = "none";
+        requestRef.current = requestAnimationFrame(
+          moveToAnim({ path, dirX, dirY, count, startCharLeft, startCharTop })
+        );
+      }
+    };
+
+  const startAnimation = (startX, startY, endX, endY, node) => {
+    if (charRef.current && gridRef.current) {
+      const dirX = Math.sign(endX - startX);
+      const dirY = Math.sign(endY - startY);
+      if (dirX === 0 && dirY === 0) {
+        console.log(`[=] you are already there!`);
+        return;
+      }
+      if (!node.walkable) {
+        console.log(`[=] node not walkable!`);
+        return;
+      }
+
+      const startCharLeft = parseInt(charRef.current.style.left);
+      const startCharTop = parseInt(charRef.current.style.top);
+
+      const path = getPath(startX, startY, endX, endY);
+      setNMoves((prevMoves) => prevMoves + path.length - 1);
+      requestRef.current = requestAnimationFrame(
+        moveToAnim({
+          path,
+          dirX,
+          dirY,
+          count: 0,
+          startCharLeft,
+          startCharTop,
+        })
+      );
+    }
+  };
+
+  return {
+    cancelAnimation,
+    resetAnimation,
+    moveToAnim,
+    startAnimation,
+    nMoves,
+  };
+};
+
+export default useAnimation;
